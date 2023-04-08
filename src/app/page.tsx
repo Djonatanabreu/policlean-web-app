0e7c3995af2147115a749f86086025fc3865b4b2
Djonatan Abreu <djonatan@Djonatans-MacBook-Air.local>
2023-04-08 00:10:39 -0300

    fix: Starting With HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+'use client';
 import styles from './page.module.css';
+import { BreadCrumb, ProductDetail } from '@/components';
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <section className={styles.productPageContent}>
+        <BreadCrumb />
+        <ProductDetail />
+      </section>
+    </main>
+  );
 }
